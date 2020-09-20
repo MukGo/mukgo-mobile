99862def57e5f0b841f87bc25078172b1aeb4b30
chungkang <chungkang.c@gmail.com>
2020-09-20 21:29:31 +0900

    axios change

--- src/components/ChannelList.js
+++ src/components/ChannelList.js
@@ -1,26 +1,32 @@
-import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Image, Linking, StyleSheet, Text, View } from 'react-native';
 
+import { Fontisto } from '@expo/vector-icons';
 import React from 'react';
 
 const ChannelList = ({ title, results }) => {
   return (
     <View>
-
       <Text style={styles.title}>{title}</Text>
-
       <FlatList
         data={results}
         keyExtractor={(results) => results.id}
         renderItem={({ item }) => {
-          return (
-            <View>
-              <Text key={item.id} >{item.snippet.title}</Text>
 
+          return (
+            <View style={styles.row}>
               <Image
+                style={styles.tinyLogo}
                 source={{
-                  uri: item.snippet.thumbnails.default.url
+                  uri: `${item.snippet.thumbnails.default.url}`,
                 }}
               />
+              <Text key={item.id} >{item.snippet.title} </Text>
+              <Fontisto
+                name="youtube-play"
+                style={styles.icon}
+                onPress={() => Linking.openURL(`https://www.youtube.com/channel/${item.snippet.resourceId.channelId}`)}
+              />
+
             </View>
           )
         }}
@@ -33,7 +39,23 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 18,
     fontWeight: 'bold'
-  }
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: 'gray'
+  },
+  icon: {
+    fontSize: 24,
+    color: 'red'
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
 });
 
 export default ChannelList;
